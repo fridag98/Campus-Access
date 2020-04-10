b2aa8d7dd34af9c884f6dd6bb7f5a767063d963b
Fridagtt <fridagtzm@gmail.com>
2020-04-09 20:24:19 -0500

    Initial Commit

---
+++ CampusAccess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CampusAccess
+//
+//  Created by Frida Gutiérrez Mireles on 09/04/20.
+//  Copyright © 2020 Frida Gutiérrez Mireles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
