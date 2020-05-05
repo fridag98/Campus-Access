6218235b339670fe0d58e74adc0276412cfc904d
Fridagtt <fridagtzm@gmail.com>
2020-05-05 10:53:00 -0500

    Group files

---
+++ CampusAccess/loginControllers/HomeStudentsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeStudentsViewController.swift
+//  CampusAccess
+//
+//  Created by Frida Gutiérrez Mireles on 05/05/20.
+//  Copyright © 2020 Frida Gutiérrez Mireles. All rights reserved.
+//
+
+import UIKit
+
+class HomeStudentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
