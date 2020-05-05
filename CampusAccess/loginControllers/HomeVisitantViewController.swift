9d3e2371ff98dcbbeea4fce336d7a2dbdb7f523b
Fridagtt <fridagtzm@gmail.com>
2020-05-05 13:33:55 -0500

    Login and SignUp Views

---
+++ CampusAccess/loginControllers/HomeVisitantViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVisitantViewController.swift
+//  CampusAccess
+//
+//  Created by Frida Gutiérrez Mireles on 05/05/20.
+//  Copyright © 2020 Frida Gutiérrez Mireles. All rights reserved.
+//
+
+import UIKit
+
+class HomeVisitantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CampusAccess/loginControllers/SigUpViewController.swift
+++ CampusAccess/loginControllers/SigUpViewController.swift
@@ -10,21 +10,13 @@ import UIKit
 
 class SigUpViewController: UIViewController {
 
+    @IBOutlet weak var logo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        logo.image = UIImage(named: "logoPrueba")!
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
