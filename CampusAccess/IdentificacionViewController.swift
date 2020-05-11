21fc3125a2f14c448ebe311d53b6370587dee154
Fridagtt <fridagtzm@gmail.com>
2020-05-10 20:34:09 -0500

    Upload identification image from gallery

---
+++ CampusAccess/IdentificacionViewController.swift
@@ -0,0 +1,55 @@
+//
+//  IdentificacionViewController.swift
+//  CampusAccess
+//
+//  Created by Frida Gutiérrez Mireles on 10/05/20.
+//  Copyright © 2020 Frida Gutiérrez Mireles. All rights reserved.
+//
+
+import UIKit
+
+
+class IdentificacionViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var identificacion : UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        identificacion = nil
+    }
+
+    //el textfield tienen un botton invisible, la cámara tienen un tap
+    @IBAction func subirFoto(_ sender: Any) {
+        let picker = UIImagePickerController()
+         picker.delegate = self
+         picker.sourceType = .photoLibrary
+         
+         present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let foto = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        identificacion = foto
+        dismiss(animated:true, completion:nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated:true, completion:nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+            let vistaSignUp = segue.destination as! SigUpViewController
+            vistaSignUp.imgIdentificacion = identificacion
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identificacion == nil {
+                let alerta = UIAlertController(title: "Error", message: "Selecciona una imagen como identificación", preferredStyle: .alert)
+                alerta.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                present(alerta, animated: true, completion: nil)
+                return false
+        }
+        return true
+    }
+
+}
--- CampusAccess/loginControllers/SigUpViewController.swift
+++ CampusAccess/loginControllers/SigUpViewController.swift
@@ -20,6 +20,8 @@ class SigUpViewController: UIViewController {
     @IBOutlet weak var tfContrasena: UITextField!
     @IBOutlet weak var btnSignUp: UIButton!
     
+    var imgIdentificacion : UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         logo.image = UIImage(named: "logoPrueba")!
@@ -27,16 +29,20 @@ class SigUpViewController: UIViewController {
     }
     
     func isPasswordValid(_ password: String) -> Bool{
-        //contraseña largo de minimo 8
+        //contraseña de longitud mínima de 8
         let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
         return passwordTest.evaluate(with: password)
     }
     
-    //regresar nil o un error message
+    //regresa nil o un error message
     func validateFields() -> String? {
-        //validar todo los campos llenos
+        //validar los datos llenos
         if (tfNombre.text == "" || tfApellido.text! == "" || tfCorreo.text! == "" || tfContrasena.text! == ""){
             return "Todos los datos deben ser llenados."
+        }
+        //validar que haya foto de identificacion
+        if(imgIdentificacion==nil){
+            return "Es necesario subir una identificación."
         }
         
         //validar si la contraseña es segura
@@ -44,6 +50,13 @@ class SigUpViewController: UIViewController {
             return "La contraseña debe de tener al menos 8 caracteres, 1 número y 1 caracter especial."
         }
         return nil
+    }
+    
+    func showError(_ message:String){
+        let alert = UIAlertController(title: "Campus Access", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func signUpTapped(_ sender: Any) {
@@ -58,7 +71,14 @@ class SigUpViewController: UIViewController {
                     self.showError("No se pudo crear la cuenta")
                 } else{
                     let db = Firestore.firestore()
-                    db.collection("visitantes").addDocument(data: ["firstname":self.tfNombre.text!, "lastname":self.tfApellido.text!, "uid": result!.user.uid]) { (error) in
+                    var dataUser: Dictionary<String, Any> = [
+                        "firstname": self.tfNombre.text!,
+                        "lastname": self.tfApellido.text!,
+                        "email": result!.user.email!,
+                        "identificacionURL": "",
+                        "uid": result!.user.uid
+                    ]
+                    db.collection("visitantes").addDocument(data: dataUser) { (error) in
                         if error != nil {
                             print("No se guardaron los datos del usuario")
                         }
@@ -70,17 +90,13 @@ class SigUpViewController: UIViewController {
         }
     }
     
-    func showError(_ message:String){
-        let alert = UIAlertController(title: "Campus Access", message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-    
     func transitionToMenu(){
         let visitorsMenu = storyboard?.instantiateViewController(withIdentifier: "visitorsMenu") as? HomeVisitantViewController
         view.window?.rootViewController = visitorsMenu
         view.window?.makeKeyAndVisible()
+    }
+
+    @IBAction func unwindIdentificacion(unwindSegue : UIStoryboardSegue){
     }
     
     @IBAction func btnInicio(_ sender: Any) {
